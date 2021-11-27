72af23f5791749e718d0f93b9df1feb7c58de8d2
lunay0ung <lunay0ung@github.com>
2021-11-27 22:25:38 +0900

    second activity 및 layout 추가, 기본 액티비티 등록

---
+++ app/src/main/java/com/luna/aac/SecondActivity.kt
@@ -0,0 +1,19 @@
+package com.luna.aac
+
+import android.app.Activity
+import android.os.Bundle
+
+class SecondActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+}
